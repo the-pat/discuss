1c56e09cb85a2fe196be5648dc1f2365102bc2e3
Patrick Tone <pt@patrickt.one>
2019-01-28 07:38:28 -0600

    Added the discussion context with topics

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -17,6 +17,7 @@ defmodule DiscussWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/topics", TopicController
   end
 
   # Other scopes may use custom stacks.
--- priv/repo/migrations/20190128132604_create_topics.exs
+++ priv/repo/migrations/20190128132604_create_topics.exs
@@ -1,9 +1,12 @@
-defmodule Discuss.Repo.Migrations.AppTopics do
+defmodule Discuss.Repo.Migrations.CreateTopics do
   use Ecto.Migration
 
   def change do
     create table(:topics) do
       add :title, :string
+
+      timestamps()
     end
+
   end
 end
