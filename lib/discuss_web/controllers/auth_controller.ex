46d19cc551372f537f8bcefe673c5e541fc7f9a3
Patrick Tone <pt@patrickt.one>
2019-02-10 13:16:26 -0600

    Added the signed in user to the session

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -8,14 +8,19 @@ defmodule DiscussWeb.AuthController do
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
     user_params = %{token: auth.credentials.token, email: auth.info.email, provider: Atom.to_string(auth.provider)}
 
+    signin(conn, user_params)
+  end
+
+  defp signin(conn, user_params) do
     case Identity.create_or_update_user(user_params) do
       {:ok, user} ->
         conn
-        |> put_flash(:info, "Logged in successful.")
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
         |> redirect(to: Routes.topic_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
         conn
-        |> put_flash(:error, "Error authenticating.")
+        |> put_flash(:error, "Error signing in.")
         |> redirect(to: Routes.topic_path(conn, :index))
     end
   end
