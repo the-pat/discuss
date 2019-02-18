e8844e6a0d2a8a2f395cfc53703b86add81f4a21
Patrick Tone <pt@patrickt.one>
2019-02-18 07:37:03 -0600

    Moved the socket salt to the config

--- config/config.exs
+++ config/config.exs
@@ -14,6 +14,7 @@ config :discuss,
 config :discuss, DiscussWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "Kx1Mm/C78o8asXTBtnyET+tFdgvk1JGmdBLeQKfa4crN0NrjqnDiMs6vuKqNppRU",
+  socket_secret_key: "I&aD$q5mFiK7",
   render_errors: [view: DiscussWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: Discuss.PubSub, adapter: Phoenix.PubSub.PG2]
 
--- lib/discuss_web/channels/user_socket.ex
+++ lib/discuss_web/channels/user_socket.ex
@@ -4,9 +4,12 @@ defmodule DiscussWeb.UserSocket do
   channel "discussion:*", DiscussWeb.DiscussionChannel
 
   def connect(%{"token" => token}, socket, _connect_info) do
-    case Phoenix.Token.verify(socket, "key", token, max_age: 86400) do
+    key = Application.get_env(:discuss, DiscussWeb.Endpoint)[:socket_secret_key]
+
+    case Phoenix.Token.verify(socket, key, token, max_age: 86400) do
       {:ok, user_id} ->
         {:ok, assign(socket, :user_id, user_id)}
+
       {:error, _error} ->
         :error
     end
